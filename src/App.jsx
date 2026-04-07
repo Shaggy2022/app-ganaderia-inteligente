08ded252fd1facefe176f80e5a77d4d12f87ce3b
Shaggy2022 <jose.mayorga1475@gmail.com>
2026-04-06 23:34:24 -0500

    Se suben últimos cambios de la HU12 temas de validaciones, reportes y demás

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Animals from "./pages/Animals";
-import Feeding from "./pages/Feeding";
 import Costs from "./pages/Costs";
 import Inventory from "./pages/Inventory";
 import ReporteFinanciero from "./pages/ReporteFinanciero";
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/animals" element={<Animals />} />
-          <Route path="/feeding" element={<Feeding />} />
           <Route path="/costs" element={<Costs />} />
           <Route path="/inventory" element={<Inventory />} />
           <Route path="/reporte-financiero" element={<ReporteFinanciero />} />
